b837dc14b6bf84aa68f423bbf9af7602b070a0f8
James Little <littleguy23@gmail.com>
2021-05-03 09:11:51 -0700

    Run fuzzy word generation on search, not build

--- src/index_versions/v3/builder/mod.rs
+++ src/index_versions/v3/builder/mod.rs
@@ -13,8 +13,6 @@ mod annotated_words_from_string;
 pub mod errors;
 pub mod intermediate_entry;
 
-mod reverse_levenshtein;
-
 use colored::Colorize;
 use fill_containers::fill_containers;
 use fill_intermediate_entries::fill_intermediate_entries;
